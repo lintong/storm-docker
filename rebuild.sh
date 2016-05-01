8e295c6bd66412d9eeec34b582cb6863f2bd6e46
Linton Galloway <mail@linton.me>
2016-05-01 08:59:46 +0100

    Upgraded to use Ubuntu 15.04 and OpenJDK 8, still need to upgrade to Apache Storm 1.0.0

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
-docker build -t="wurstmeister/storm-nimbus" storm-nimbus
-docker build -t="wurstmeister/storm-supervisor" storm-supervisor
-docker build -t="wurstmeister/storm-ui" storm-ui
+docker build -t="lintong/storm:1.0.0" storm
+docker build -t="lintong/storm-nimbus:1.0.0" storm-nimbus
+docker build -t="lintong/storm-supervisor:1.0.0" storm-supervisor
+docker build -t="lintong/storm-ui:1.0.0" storm-ui
